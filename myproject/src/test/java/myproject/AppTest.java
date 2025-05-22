c543b7b3155796723315f297c722861f08c4e067
Giulia Giamberini <giuliagiamberini@gmail.com>
2025-05-22 18:11:39 +0200

    Fixed Coverage on App.sayHello if-else

--- myproject/src/test/java/myproject/AppTest.java
+++ myproject/src/test/java/myproject/AppTest.java
@@ -18,4 +18,10 @@ public class AppTest {
 	public void testSayHello() {
 		assertEquals("Hello", app.sayHello());
 	}
+	
+	@Test
+	public void testSayHelloWithName() {
+		assertEquals("Hello World", app.sayHello("World"));
+		
+	}
 }
